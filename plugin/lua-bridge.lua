daf007be94301f8437995362459f525099ef14cc
Rob Hoelz <rob@hoelz.ro>
2011-09-08 21:39:54 -0500

    Add vim.bridge module helper

--- plugin/lua-bridge.lua
+++ plugin/lua-bridge.lua
@@ -16,3 +16,12 @@
 -- COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
 -- AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 -- WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+local bridge_env = {}
+
+function vim.bridge(module)
+  setmetatable(module, {
+    __metatable = false,
+    __index     = bridge_env,
+  })
+end
