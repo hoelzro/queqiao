1ad505c1f58d0dc204a070188bfe603cb8bba286
Rob Hoelz <rob@hoelz.ro>
2011-09-11 13:11:31 -0500

    Treat Lua functions as Vim functions

--- plugin/lua-bridge.lua
+++ plugin/lua-bridge.lua
@@ -19,6 +19,12 @@
 
 local setmetatable = setmetatable
 local bridge_env   = setmetatable({}, { __index = _G }) -- for now
+local type         = type
+local rawset       = rawset
+local smatch       = string.match
+local format       = string.format
+
+_G.function_registry = {}
 
 _G.bridge_env = bridge_env
 util          = {}
@@ -37,8 +43,30 @@ _G.bridge_env = nil
 util          = nil
 
 function vim.bridge(module)
+  local function_storage = setmetatable({}, { __index = bridge_env })
+
   setmetatable(module, {
     __metatable = false,
-    __index     = bridge_env,
+    __index     = function_storage,
+    __newindex  = function(self, k, v)
+      local t = type(v)
+
+      if t == 'function' then
+        if smatch(k, '^%u') then
+          _G.function_registry[#_G.function_registry + 1] = v -- XXX cleanup?
+          local id = #_G.function_registry
+
+          -- XXX register function
+          vim.command(format([[
+function! %s()
+  lua _G.function_registry[%d]()
+endfunction
+          ]], k, id))
+        end
+        rawset(function_storage, k, v)
+      else
+        rawset(self, k, v)
+      end
+    end
   })
 end
