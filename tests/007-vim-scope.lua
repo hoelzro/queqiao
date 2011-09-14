fba2e7c68641e3b84b011b7c68d86f0bba2cf3eb
Rob Hoelz <rob@hoelz.ro>
2011-09-13 23:58:42 -0500

    Create special test for Vim scope

--- tests/007-vim-scope.lua
+++ tests/007-vim-scope.lua
@@ -1,4 +1,20 @@
 scope      = 'vim'
 scope_abbr = 'v'
 
-dofile 'scope-tests.lua'
+local realvim = vim
+
+module('scope_test', vim.bridge)
+
+local env = getfenv(1)
+
+for _, scope in ipairs { env[scope], env[scope_abbr] } do
+  scope.statusmsg = 'bar'
+  realvim.command('echo ' .. scope_abbr .. ':statusmsg')
+  scope.statusmsg = 189
+  realvim.command('echo ' .. scope_abbr .. ':statusmsg')
+
+  realvim.command('let ' .. scope_abbr .. ':statusmsg = 179')
+  print(scope.statusmsg)
+  realvim.command('let ' .. scope_abbr .. ':statusmsg = "baz"')
+  print(scope.statusmsg)
+end
