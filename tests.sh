e5f3395b3ade9cc9dee9f5cc46ede6bbcf1e61b7
Rob Hoelz <rob@hoelz.ro>
2011-09-14 18:27:05 -0500

    Allow tests.sh to run an individual test

--- tests.sh
+++ tests.sh
@@ -2,15 +2,17 @@
 
 which runVimTests.sh >/dev/null 2>&1
 
-if [[ "x$1" == "xclean" ]]; then
-    rm tests/*.msgout
-    rm tests/*.msgresult
+if [[ $? -ne 0 ]]; then
+    echo "You need runVimTests.sh installed to run the test suite"
+    echo "http://www.vim.org/scripts/script.php?script_id=2565"
     exit
 fi
 
-if [[ $? -eq 0 ]]; then
-    runVimTests.sh --pure tests/
+if [[ "x$1" == "xclean" ]]; then
+    rm tests/*.msgout
+    rm tests/*.msgresult
+elif [[ "x$1" != "x" ]]; then
+    runVimTests.sh --pure "$1"
 else
-    echo "You need runVimTests.sh installed to run the test suite"
-    echo "http://www.vim.org/scripts/script.php?script_id=2565"
+    runVimTests.sh --pure tests/
 fi
